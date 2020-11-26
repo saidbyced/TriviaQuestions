f9dc08b232d43ec320d89c9b905838c3404efb2a
Chris Eadie <chris.eadie@outlook.com>
2020-11-26 22:10:59 +0000

    Add variables to store API request response and make request on view load

--- TriviaQuestions/ViewController.swift
+++ TriviaQuestions/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let triviaAPIRequest = TriviaRequest()
+    var triviaItems = [TriviaItem]() {
+        didSet {
+            updateUI()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        triviaAPIRequest.getTrivia { [weak self] result in
+            switch result {
+            case .failure(let error):
+                print(error)
+            case .success(let triviaItems):
+                self?.triviaItems = triviaItems
+            }
+        }
     }
-
+    
+    func updateUI() {
+        // Set state of UI
+    }
 
 }
 
